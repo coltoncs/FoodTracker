5cb9167630b89801f9d994df59147d8993d29e38
Colton Sweeney <csweeney98@gmail.com>
2017-08-08 23:53:01 -0400

    initial project commit.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Here's the start of the app")
     }
 
     override func didReceiveMemoryWarning() {
